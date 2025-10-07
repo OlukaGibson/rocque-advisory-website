7f58190ae091fc25dbd8f88bfb96387451c67b1d
OlukaGibson <gibsonoluka7@gmail.com>
2025-10-07 13:07:13 +0300

    minor edits

--- src/shared/seo.ts
+++ src/shared/seo.ts
@@ -4,7 +4,7 @@ export const META_TAGS = {
   description: 'We are a development consulting firm that supports international organisations and their recipients deliver effective programmes',
   ogTitle: 'Rocque Advisory',
   ogDescription: 'Building Bridges for Effective Development Assistance',
-  ogImage: '/assets/images/og-image.jpg',
+  ogImage: '/assets/images/og-image.jpeg',
   ogType: 'website',
   ogUrl: 'https://rocqueadvisory.com'
 };
